1b432946dce8cd28493eb77bb263eeee21fb0b22
Juan Hevilla <j.hevilla@diagram.es>
2022-04-14 11:39:24 +0200

    Docx/PDF según extensión

--- impreduplex.py
+++ impreduplex.py
@@ -7,20 +7,33 @@ import glob
 import platform
 import subprocess
 import tempfile
+import base64
 from PIL import Image
 from pdf2image import convert_from_path
 from fpdf import FPDF
-
+from docx import Document
+from docx.shared import Cm
+from docx.enum.table import WD_CELL_VERTICAL_ALIGNMENT
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 if platform.system() == 'Windows':
     import win32print
     import win32event
     import win32process
     import win32con
+    import win32com.client
     from win32com.shell.shell import ShellExecuteEx
     from win32com.shell import shellcon
 
 __version__ = '0.0.1'
 appName = 'impreduplex'
+
+
+img_relleno = """
+iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBI
+WXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3wYHFSo7+aaN1AAAADNJREFUOMtj/P//PwMlgImBQkCx
+ASzInJ07dxKlyd3dfRB5YdSAYWEA44Blpt+/f1PHBQDWcw8IAHFJDwAAAABJRU5ErkJggg==
+"""
+img_relleno = BytesIO(base64.b64decode(img_relleno.replace('\n', '')))
 
 
 def win_duplex(nom_imp, duplex=3):
@@ -74,6 +87,24 @@ def win_imprime(docu, impresora, duplex):
         win_duplex(impresora, duplex_ant)
 
 
+def win_get_paginas(documento):
+    # TODO:: Para corregir el error
+    # win32com\client\CLSIDToClass.pyc: KeyError: '{00020970-0000-0000-C000-000000000046}'
+    if win32com.client.gencache.is_readonly:
+        win32com.client.gencache.is_readonly = False
+        win32com.client.gencache.Rebuild()  # create gen_py folder if needed
+
+    # word = win32com.client.Dispatch('Word.Application')
+    word = win32com.client.gencache.EnsureDispatch('Word.Application')
+    # word.Visible = False
+    w = word.Documents.Open(documento)
+    w.Repaginate()
+    paginas = w.ComputeStatistics(2)
+    w.Close()
+    word.Quit()
+    return paginas
+
+
 def escala_imagen(imagen, width, height):
     try:
         im = Image.open(imagen)
@@ -103,7 +134,7 @@ class MiFPDF(FPDF):
         self.cell(0, 10, f'Página {self.page_no()}' + '/{nb}', 0, 0, 'C')
 
 
-def albaranes(pdf, albaranes_img, albaran, img_pag_ancho, img_pag_alto):
+def albaranes_pdf(pdf, albaranes_img, albaran, img_pag_ancho, img_pag_alto):
     pdf.add_page()
     posy = 0
     for alto in range(0, img_pag_alto):
@@ -131,16 +162,89 @@ def crea_pdf(facturas_img, albaranes_img, doc_destino, img_pag_ancho, img_pag_al
         pdf.add_page()
         pdf.image(factura, 0, 0, pdf.fw, pdf.fh)
         if albaran < len(albaranes_img):
-            albaran = albaranes(pdf, albaranes_img, albaran, img_pag_ancho, img_pag_alto)
+            albaran = albaranes_pdf(pdf, albaranes_img, albaran, img_pag_ancho, img_pag_alto)
 
     while albaran < len(albaranes_img):
-        albaran = albaranes(pdf, albaranes_img, albaran, img_pag_ancho, img_pag_alto)
+        albaran = albaranes_pdf(pdf, albaranes_img, albaran, img_pag_ancho, img_pag_alto)
 
     # siempre pares
     if pdf.page_no() % 2 != 0:
         pdf.add_page()
 
     pdf.output(doc_destino)
+
+
+def albaranes(document, albaranes_img, albaran, img_pag_ancho, img_pag_alto, twidth, theight):
+    twidth -= .5
+    theight -= img_pag_alto / 2 if platform.system() == 'Windows' else img_pag_alto  # para que no haga saltos de página
+    table = document.add_table(rows=img_pag_alto, cols=img_pag_ancho)
+    table.allow_autofit = False
+    table.style = None
+    for alto in range(0, img_pag_alto):
+        for ancho in range(0, img_pag_ancho):
+            cell = table.rows[alto].cells[ancho]
+            cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER
+            paragraph = cell.paragraphs[0]
+            paragraph.style = None
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+            run = paragraph.add_run()
+            if albaran < len(albaranes_img):
+                width = twidth / img_pag_ancho
+                height = theight / img_pag_alto
+                imagen = albaranes_img[albaran]
+                w, h = escala_imagen(imagen, width, height)
+                run.add_picture(img_relleno, width=Cm(width), height=Cm((height-h)/2))
+                run.add_picture(imagen, width=Cm(w), height=Cm(h))
+                run.add_picture(img_relleno, width=Cm(width), height=Cm((height-h))/2)
+                albaran = albaran + 1
+    return albaran
+
+
+def crea_docx(facturas_img, albaranes_img, doc_destino, img_pag_ancho, img_pag_alto):
+    document = Document('impreduplex.docx')
+    width = document.sections[0].page_width.cm
+    height = document.sections[0].page_height.cm
+    top = document.sections[0].top_margin.cm
+    bottom = document.sections[0].bottom_margin.cm
+    left = document.sections[0].left_margin.cm
+    right = document.sections[0].right_margin.cm
+    twidth = width - left - right
+    theight = height - top - bottom
+
+    for paragraph in document.paragraphs:
+        p = paragraph._element
+        p.getparent().remove(p)
+        p._p = p._element = None
+
+    paginas = albaran = 0
+    for factura in facturas_img:
+        paginas += 1
+        document.add_picture(factura, width=Cm(twidth), height=Cm(theight))
+        if albaran < len(albaranes_img):
+            albaran = albaranes(document, albaranes_img, albaran, img_pag_ancho, img_pag_alto, twidth, theight)
+            paginas += 1
+
+    while albaran < len(albaranes_img):
+        albaran = albaranes(document, albaranes_img, albaran, img_pag_ancho, img_pag_alto, twidth, theight)
+        paginas += 1
+
+    # Las páginas deben ser siempre pares
+    if platform.system() == 'Windows':
+        # en windows se cuentan las páginas con el Word
+        document.save(doc_destino)
+        paginas = win_get_paginas(doc_destino)
+        print(f'Páginas creadas: {paginas}')
+        if paginas % 2 != 0:
+            document = Document(doc_destino)
+            document.add_page_break()
+            document.save(doc_destino)
+            paginas = win_get_paginas(doc_destino)
+            print(f'Página añadida, total: {paginas}')
+    else:
+        if paginas % 2 != 0:
+            document.add_page_break()
+            # document.add_paragraph('.')
+        document.save(doc_destino)
 
 
 def main():
@@ -179,7 +283,10 @@ def main():
         else:
             albaranes_img.append(BytesIO(data))
 
-    crea_pdf(facturas_img, albaranes_img, doc_destino, img_pag_ancho, img_pag_alto)
+    if doc_destino.lower().endswith('.pdf'):
+        crea_pdf(facturas_img, albaranes_img, doc_destino, img_pag_ancho, img_pag_alto)
+    else:
+        crea_docx(facturas_img, albaranes_img, doc_destino, img_pag_ancho, img_pag_alto)
 
     if impresora:
         if platform.system() == 'Darwin':       # mac
--- run.bat
+++ run.bat
@@ -1,2 +1,13 @@
-
-dist\impreduplex.exe imagenes\Factura.pdf imagenes\D*.* doc.pdf 2 4 ver
+@echo off
+set /p respuesta=Tipo de archivo Docx o PDF?(d/p):
+IF %respuesta%==d goto docx
+IF %respuesta%==p goto pdf
+goto fin
+:docx
+@echo on
+dist\impreduplex.exe imagenes\Factura.pdf imagenes\D*.* c:\tmp\doc.docx 2 4 ver
+goto fin
+:pdf
+@echo on
+dist\impreduplex.exe imagenes\Factura.pdf imagenes\D*.* c:\tmp\doc.pdf 2 4 ver
+:fin
